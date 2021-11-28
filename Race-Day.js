0eeb314e1f541137f3cb62a86224e7eb3959e864
Krystian Kruszelnicki <jack150tw@gmail.com>
2021-11-28 17:22:35 +0100

    Update 28.11.2021

---
+++ Race-Day.js
@@ -0,0 +1,16 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+let registeredEarly = true;
+let runnersAge = 18;
+
+if (registeredEarly && (runnersAge >= 18) ) {
+  raceNumber = raceNumber + 1000;
+} else {
+  raceNumber;
+}
+if (registeredEarly && (runnersAge >= 18)){
+  console.log(`Your race is at 9:30 am and your number is ${raceNumber}`)
+} else if (runnersAge >= 18) {
+  console.log(`Your race is at 11:00 am and your number is ${raceNumber}`);
+} else {
+  console.log(`Your race is at 12:30 pm and your number is ${raceNumber}`);
+}
