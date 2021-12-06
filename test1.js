dc7fa248cce1e96fd08ccedf12a094b6f7085963
Krystian Kruszelnicki <jack150tw@gmail.com>
2021-12-06 23:19:02 +0100

    Update 06.12.2021

--- test1.js
+++ test1.js
@@ -1,1 +1,22 @@
-console.log(typeof("Hola"));
+let a = prompt("gwgweg");
+let b = prompt("gwgweg");
+let c = prompt("gwgweg");
+function add(a,b,c) {
+    if (a === undefined || typeof a === "string") {a = 0} else {a=a};
+    if (b === undefined || typeof b === "string") {b = 0} else {b=b};
+    if (c === undefined || typeof c === "string") {c = 0} else {c=c};
+    console.log(`${a} + ${b} + ${c}`);
+    return a+b+c;
+}
+console.log(add(a,b,c));
+
+
+/*function add(a,b,c) {
+    a = a || 0;
+    b = b || 0;
+    c = c || 0;
+    console.log(`${a} + ${b} + ${c}`);
+    return a+b+c;
+}
+console.log(add());
+*/
